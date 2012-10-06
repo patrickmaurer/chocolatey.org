ed125e29ebad3079696c5fbb17126de5ac39a18f
Rob Reynolds <ferventcoder@gmail.com>
2012-10-06 07:22:49 -0500

    holding point for adding notices

---
+++ chocolatey/Website/ViewModels/PackageBrokenViewModel.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace NuGetGallery
+{
+    public class PackageBrokenViewModel
+    {
+        public string PackageId { get; set; }
+        public string PackageVersion { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        [Display(Name = "Operating system information")]
+        public string WindowsOsInformation { get; set; }
+
+        [Required]
+        [StringLength(4000)]
+        [Display(Name = "Error Message")]
+        public string Message { get; set; }
+        
+        [Required(ErrorMessage = "Please enter your email address.")]
+        [StringLength(4000)]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"(?i)^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$", ErrorMessage = "This doesn't appear to be a valid email address.")]
+        public string Email { get; set; }
+
+        public bool ConfirmedUser { get; set; } 
+    }
+}
